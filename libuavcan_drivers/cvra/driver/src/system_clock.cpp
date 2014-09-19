b5410e3869b44859f1758c01ccdabc7b1c152cf6
Patrick Spieler <stapelzeiger@gmail.com>
2014-09-19 21:25:19 +0200

    fix system_clock driver
    compiles & links now without errors

--- libuavcan_drivers/cvra/driver/src/system_clock.cpp
+++ libuavcan_drivers/cvra/driver/src/system_clock.cpp
@@ -1,28 +1,25 @@
 #include <uavcan_cvra/can.hpp>
+#include <uavcan_cvra/system_clock.hpp>
 
 namespace uavcan_cvra
 {
 
-class SystemClock : uavcan::ISystemClock
-{
-
 static uavcan::uint32_t time_us;
 
-uavcan::MonotonicTime getMonotonic() const
+uavcan::MonotonicTime SystemClock::getMonotonic() const
 {
     return uavcan::MonotonicTime::fromUSec(time_us++);
 }
 
-uavcan::UtcTime getUtc() const
+uavcan::UtcTime SystemClock::getUtc() const
 {
     return uavcan::UtcTime::fromUSec(time_us);
 }
 
-void adjustUtc(uavcan::UtcDuration adjustment)
+void SystemClock::adjustUtc(uavcan::UtcDuration adjustment)
 {
 
 }
 
-};
 
 }  /* namespace uavcan_cvra */
